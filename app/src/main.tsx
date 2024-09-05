8170b64acbc44741ccf4071f0fa73bc43b6b4020
lucianobcorrea <lucianobalestrincorrea@gmail.com>
2024-09-04 21:05:21 -0300

    fix: corrigindo o arquivo app e instalando corretamente o tailwind

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
 import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
