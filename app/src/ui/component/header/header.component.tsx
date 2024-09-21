155a84865ff1f2d36abf333109457c88c6fb6acf
Luciano Correa <lucianobalestrincorrea@gmail.com>
2024-09-21 16:18:39 -0300

    feat: adicionando parte do perfil

--- app/src/ui/component/header/header.component.tsx
+++ app/src/ui/component/header/header.component.tsx
@@ -10,10 +10,10 @@ import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <header className="h-32 border-b-[1px] border-neutral-700 bg-neutral-850 flex items-center">
-      <div className="container">
-        <div>
+      <div className="container flex justify-between items-center p-0">
+        <div className="flex items-center gap-9">
           <Link to="/menu">
-            <div className="flex items-center gap-3 bg-neutral-700 w-fit px-6 py-3 rounded-lg">
+            <div className="flex items-center gap-3 bg-neutral-700 w-fit px-6 py-2 rounded-lg hover:bg-neutral-600  transition ease-in-out">
               <img
                 className="max-h-11 max-w-11"
                 src={config}
@@ -22,9 +22,54 @@ export const Header = () => {
               <p className="text-white font-bold text-xl">Menu</p>
             </div>
           </Link>
+
+          <Link to="/perfil">
+            <div className="flex items-center gap-3 bg-neutral-700 w-fit px-6 py-2 rounded-lg hover:bg-neutral-600  transition ease-in-out">
+              <img
+                className="max-h-11 max-w-11"
+                src={profile}
+                alt="Ilustração de uma cabeça de um menino"
+              />
+              <p className="text-white font-bold text-xl">Perfil</p>
+            </div>
+          </Link>
+
+          <Link to="/loja">
+            <div className="flex items-center gap-3 bg-neutral-700 w-fit px-6 py-2 rounded-lg hover:bg-neutral-600  transition ease-in-out">
+              <img
+                className="max-h-11 max-w-11"
+                src={shop}
+                alt="Carrinho de compras com uma preguiça dentro"
+              />
+              <p className="text-white font-bold text-xl">Loja</p>
+            </div>
+          </Link>
+
+          <Link to="/missoes">
+            <div className="flex items-center gap-3 bg-neutral-700 w-fit px-6 py-2 rounded-lg hover:bg-neutral-600  transition ease-in-out">
+              <img
+                className="max-h-11 max-w-11"
+                src={missions}
+                alt="Livro aberto"
+              />
+              <p className="text-white font-bold text-xl">Missões</p>
+            </div>
+          </Link>
         </div>
 
-        <div></div>
+        <div className="flex items-center gap-10">
+          <div className="flex items-center gap-2">
+            <img className="max-h-6 max-w-6" src={fire} alt="Chama" />
+            <p className="text-secondary-color font-bold text-xl">20 dias</p>
+          </div>
+
+          <span className="text-neutral-700 font-medium text-xl">|</span>
+
+          <div className="flex items-center gap-2">
+            <p className="text-secondary-color font-bold text-xl">2134</p>
+            <img className="max-h-6 max-w-6" src={coin} alt="Moeda dourada" />
+          </div>
+        </div>
       </div>
     </header>
   );
--- app/src/ui/screen/profile/profile.screen.tsx
+++ app/src/ui/screen/profile/profile.screen.tsx
@@ -1,9 +1,43 @@
 import { Main } from "@/ui/layouts/main.layout";
+import banner from "@/assets/image/profile/banner-profile.png";
+import avatar from "@/assets/image/profile/avatar.png";
+import { ButtonComponent } from "@/ui/component/button/button.component";
+import coin from "@/assets/image/general/coin.png";
+import fire from "@/assets/image/general/fire.png";
 
 export const Profile = () => {
-    return (
-        <Main>
-            
-        </Main>
-    );
-}
+  return (
+    <Main>
+      <div className="relative">
+        <img
+          className="w-full h-[600px] object-cover rounded-b-[50px]"
+          src={banner}
+          alt="Banner do usuário"
+        />
+      </div>
+      <div className="container bg-neutral-850 absolute bottom-[-6%] left-0 right-0 rounded-3xl py-10">
+        <div className="grid grid-cols-[1.3fr,2fr,0.5fr] px-24">
+          <div className="relative">
+            <div className="bg-red-500 rounded-3xl p-6 w-full h-full flex justify-center relative top-[-50%]">
+              <img
+                className="w-[300px] h-[300px] object-cover"
+                src={avatar}
+                alt="Avatar do usuário"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <h1 className="font-bold text-white text-4xl">
+              Luciano Balestrin Corrêa
+            </h1>
+          </div>
+          <div className="flex justify-end">
+            <ButtonComponent classname="bg-primary-color hover:bg-primary-color-dark hover:border-primary-color border-secondary-color">
+              Editar perfil
+            </ButtonComponent>
+          </div>
+        </div>
+      </div>
+    </Main>
+  );
+};
